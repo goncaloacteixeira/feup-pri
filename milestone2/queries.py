99cc69c3c646a227d26fe6534e75f6b3297946a9
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2021-12-12 01:42:24 +0000

    updated query

--- milestone2/queries.py
+++ milestone2/queries.py
@@ -5,9 +5,10 @@ SELECT_URL = 'http://localhost:8983/solr/movies/select'
 
 # get movies where Tobey Maguire takes part in
 params = {
-    'q': '{!parent which="-_nest_path_:* *:*"}name:"Tobey Maguire"',
+    'q': '{!parent which="-_nest_path_:* *:*"}name:"Tobey Maguire" AND role:actor',
     'fl': 'original_title, year, imdb_title_id',
-    'rows': 1000
+    'rows': 1000,
+    'sort': 'year desc'
 }
 
 movies = get(SELECT_URL, params=params)
@@ -15,11 +16,12 @@ movies_json = json.loads(movies.text)["response"]["docs"]
 
 print('Movies Tobey Maguire took part in:')
 for doc in movies_json:
-    print("> %s - %s - %s" % (doc['imdb_title_id'], doc["original_title"], doc["year"][0]))
+    print("> %s - %s - %s" % (doc['imdb_title_id'], doc["original_title"], doc["year"]))
 
-# get drama movies from the 20's, sorted by year asc and mean vote desc
+print()
+# get drama movies from the 20's with at least 8.0 as mean vote, sorted by year asc and mean vote desc
 params = {
-    'q': 'title:* AND genre:drama AND year:[1920 TO 1930]',
+    'q': 'title:* AND genre:drama AND year:[1920 TO 1930] AND mean_vote:[8.0 TO *]',
     'sort': 'year asc, mean_vote desc',
     'fl': '*,[child]',
     'rows': 1000
@@ -28,6 +30,6 @@ params = {
 movies = get(SELECT_URL, params=params)
 movies_json = json.loads(movies.text)["response"]["docs"]
 
-print('Drama movies from the 20\'s')
+print('Drama movies from the 20\'s:')
 for doc in movies_json:
     print("> %s - %s - %d - %.02f" % (doc['imdb_title_id'], doc['original_title'], doc['year'], doc['mean_vote'][0]))
