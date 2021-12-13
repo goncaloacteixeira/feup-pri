22d8bae9b226d5f6ce72e3a2464c90ed44c05fec
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2021-12-13 12:59:52 +0000

    updated schema and some queries

--- milestone2/queries.py
+++ milestone2/queries.py
@@ -3,25 +3,29 @@ import json
 
 SELECT_URL = 'http://localhost:8983/solr/movies/select'
 
-# get movies where Tobey Maguire takes part in
+# get Drama movies before 2000 Tobey Maguire took part in (sorted by vote desc)
 params = {
-    'q': '{!parent which="-_nest_path_:* *:*"}name:"Tobey Maguire" AND role:actor',
-    'fl': 'original_title, year, imdb_title_id',
-    'rows': 1000,
-    'sort': 'year desc'
+    'q': '{!parent which="-_nest_path_:* *:*"}name:"Tobey Maguire"',
+    'indent': 'true',
+    'fl': '*, [child]',
+    'q.op': 'OR',
+    'fq': ['year:[* TO 1999]',
+           'genre:drama'],
+    'sort': 'weighted_average_vote desc',
+    'wt': 'json',
 }
 
 movies = get(SELECT_URL, params=params)
 movies_json = json.loads(movies.text)["response"]["docs"]
 
-print('Movies Tobey Maguire took part in:')
+print('Drama movies before 2000 Tobey Maguire took part in (sorted by vote desc):')
 for doc in movies_json:
     print("> %s - %s - %s" % (doc['imdb_title_id'], doc["original_title"], doc["year"]))
 
 print()
-# get drama movies from the 20's with at least 8.0 as mean vote, sorted by year asc and mean vote desc
+# get comedy movies from the 20's with at least 8.0 as mean vote, sorted by year asc and mean vote desc
 params = {
-    'q': 'title:* AND genre:drama AND year:[1920 TO 1930] AND mean_vote:[8.0 TO *]',
+    'q': 'title:* AND genre:comedy AND year:[1920 TO 1930] AND mean_vote:[8.0 TO *]',
     'sort': 'year asc, mean_vote desc',
     'fl': '*,[child]',
     'rows': 1000
@@ -30,14 +34,15 @@ params = {
 movies = get(SELECT_URL, params=params)
 movies_json = json.loads(movies.text)["response"]["docs"]
 
-print('Drama movies from the 20\'s:')
+print('Comedy movies from the 20\'s:')
 for doc in movies_json:
     print("> %s - %s - %d - %.02f" % (doc['imdb_title_id'], doc['original_title'], doc['year'], doc['mean_vote'][0]))
 
-# worst 10 rated movies
+# worst 10 rated horror movies since 2000
 params = {
-    'q': 'mean_vote:*',
-    'sort': 'mean_vote asc',
+    'q': 'genre:horror year:[2000 TO *]',
+    'q.op': 'and',
+    'sort': 'weighted_average_vote asc',
     'fl': 'imdb_title_id, original_title, mean_vote',
     'rows': 10
 }
@@ -45,11 +50,10 @@ params = {
 movies = get(SELECT_URL, params=params)
 movies_json = json.loads(movies.text)["response"]["docs"]
 
-print('Worst 10 rated movies\'s')
+print('Worst 10 rated horror movies since 2000')
 for i in range(len(movies_json)):
     doc = movies_json[i]
-    print("#%d %s - %s - %.02f" % (i+1, doc['imdb_title_id'], doc['original_title'], doc['mean_vote'][0]))
-
+    print("#%d %s - %s - %.02f" % (i + 1, doc['imdb_title_id'], doc['original_title'], doc['mean_vote'][0]))
 
 # Top actors with most participations in movies (Not woking)
 params = {
@@ -64,13 +68,13 @@ params = {
 movies = get(SELECT_URL, params=params)
 groups = json.loads(movies.text)['grouped']['imdb_name_id']['groups']
 print(groups[0]['doclist']['docs'])
-#movies_json = json.loads(movies.text)["response"]["docs"]
+# movies_json = json.loads(movies.text)["response"]["docs"]
 
-#print('Top actor\'s with most participations')
-#for doc in movies_json:
+# print('Top actor\'s with most participations')
+# for doc in movies_json:
 #    print("> %s - %s - %d - %.02f" % (doc['imdb_title_id'], doc['original_title'], doc['year'], doc['mean_vote'][0]))
 
-#print(movies_json)
+# print(movies_json)
 
 
 # top 10 rated Drama movies with at least 100000 votes
@@ -87,7 +91,8 @@ movies_json = json.loads(movies.text)["response"]["docs"]
 print('Top 10 rated movies\'s')
 for i in range(len(movies_json)):
     doc = movies_json[i]
-    print("#%d %s - %s - %.02f - %d" % (i+1, doc['imdb_title_id'], doc['original_title'], doc['mean_vote'][0], doc['total_votes'][0]))
+    print("#%d %s - %s - %.02f - %d" % (
+        i + 1, doc['imdb_title_id'], doc['original_title'], doc['mean_vote'][0], doc['total_votes'][0]))
 
 # Use uf to get movies with word Crime in the title ignoring genre
 params = {
@@ -104,5 +109,32 @@ movies_json = json.loads(movies.text)["response"]["docs"]
 print('Movie\'s with the word Crime in title')
 for i in range(len(movies_json)):
     doc = movies_json[i]
-    print("#%d %s - %s - %.02f - %d" % (i+1, doc['imdb_title_id'], doc['original_title'], doc['mean_vote'][0], doc['total_votes'][0]))
+    print("#%d %s - %s - %.02f - %d" % (
+        i + 1, doc['imdb_title_id'], doc['original_title'], doc['mean_vote'][0], doc['total_votes'][0]))
+
+
+# Action movies from the 20\'s 1hour or shorter where at least one actor has New York mentioned on their
+# biography, sorted by weighted average vote descending:
+
+params = {
+    'q': '{!parent which="-_nest_path_:* *:*"}bio:"new york" role:actor',
+    'indent': 'true',
+    'fl': '*, [child]',
+    'q.op': 'AND',
+    'fq': ['year:[1920 TO 1929]',
+           'genre:action',
+           'duration:[* TO 60]'],
+    'sort': 'weighted_average_vote desc',
+    'wt': 'json'
+}
+
+movies = get(SELECT_URL, params=params)
+movies_json = json.loads(movies.text)["response"]["docs"]
+
+print('Action movies from the 20\'s 1hour or shorter where at least one actor has New York mentioned on their '
+      'biography, sorted by weighted average vote descending: ')
+for i in range(len(movies_json)):
+    doc = movies_json[i]
+    print("#%d %s - %s - %.02f - %d" % (
+        i + 1, doc['imdb_title_id'], doc['original_title'], doc['mean_vote'][0], doc['total_votes'][0]))
 
