cbb67b093e15ed95f213fadfdb783ae2cda194cc
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2021-12-15 16:07:31 +0000

    some work on backend

--- milestone3/server/index.js
+++ milestone3/server/index.js
@@ -4,9 +4,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const routes = require('./routes');
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+app.get("/api/movies/search/:query", routes.searchMovie);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
